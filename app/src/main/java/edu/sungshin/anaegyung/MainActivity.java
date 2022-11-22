e930f80c81aca63df15075327ee0494e34d5e639
허예은 <20211415@sungshin.ac.kr>
2022-11-22 21:10:48 +0900

    data read

--- app/src/main/java/edu/sungshin/anaegyung/MainActivity.java
+++ app/src/main/java/edu/sungshin/anaegyung/MainActivity.java
@@ -1,14 +1,62 @@
 package edu.sungshin.anaegyung;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import android.speech.tts.TextToSpeech;
+import static android.speech.tts.TextToSpeech.ERROR;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+    public String TAG="TAG";
+    private DatabaseReference mDatabaseRef;
+    private TextToSpeech tts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tts=new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int i) {
+                if(i!=ERROR){
+                    tts.setLanguage(Locale.KOREAN);
+                }
+            }
+        });
+
+        mDatabaseRef= FirebaseDatabase.getInstance().getReference("Anaegyung");
+
+        mDatabaseRef.child("TestAccount").child("info").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.getValue(TestAccount.class)!=null){
+                    TestAccount testAccount=snapshot.getValue(TestAccount.class);
+                    Log.d("TAG",String.valueOf(testAccount.getArea()));
+                    Log.d("TAG",String.valueOf(testAccount.getDirect()));
+                } else{
+                    Toast.makeText(MainActivity.this, "데이터 없음", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        tts.speak("테스트",TextToSpeech.QUEUE_FLUSH, null);
+
+
     }
 }
---
+++ app/src/main/java/edu/sungshin/anaegyung/UserAccount.java
@@ -0,0 +1,16 @@
+package edu.sungshin.anaegyung;
+
+public class UserAccount {
+    private int index;
+    private String objectName;
+    private int area;
+    private int direction;
+
+    public UserAccount(){ }
+
+    public int getArea() { return area; }
+    public void setArea(int area) { this.area = area; }
+    public int getDirection() { return direction; }
+    public void setDirection(int direction) { this.direction = direction; }
+}
+
